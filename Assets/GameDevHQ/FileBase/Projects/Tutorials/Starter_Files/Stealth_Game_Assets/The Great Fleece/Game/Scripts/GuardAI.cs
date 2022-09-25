f2aae2472e1cfde6f415fd892e9c140606fdcf15
SuleimanAbdullah <suleimanabdullahdev@gmail.com>
2022-09-25 14:56:15 +0300

    Guard 1 move to point A using navmesh agent

--- Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/Stealth_Game_Assets/The Great Fleece/Game/Scripts/GuardAI.cs
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/Stealth_Game_Assets/The Great Fleece/Game/Scripts/GuardAI.cs
@@ -1,11 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class GuardAI : MonoBehaviour
 {
     [SerializeField]
     private List<Transform> _wayPoints;
+    [SerializeField]
+    private Transform _currentWayPoint;
+    private NavMeshAgent _agent;
 
-  
+    void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+
+        if (_wayPoints.Count > 0)
+        {
+            if (_wayPoints[0] != null)
+            {
+                _currentWayPoint = _wayPoints[0];
+                _agent.SetDestination(_currentWayPoint.position);
+            }
+        }
+
+    }
+
+
 }
