bf4cf4286f30714d7ce85848b23657c6358c47d8
SuleimanAbdullah <suleimanabdullahdev@gmail.com>
2022-09-26 10:54:18 +0300

    Undrstand Sloopy waypoint before make modular waypoint script

--- Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/Stealth_Game_Assets/The Great Fleece/Game/Scripts/GuardAI.cs
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/Stealth_Game_Assets/The Great Fleece/Game/Scripts/GuardAI.cs
@@ -23,8 +23,28 @@ public class GuardAI : MonoBehaviour
                 _agent.SetDestination(_currentWayPoint.position);
             }
         }
-
     }
 
+    void Update()
+    {
+        if (_currentWayPoint != null)
+        {
+            float distance = Vector3.Distance(transform.position, _currentWayPoint.position);
+            if (distance < 1.0f)
+            {
+                if (_wayPoints[1] != null && _currentWayPoint == _wayPoints[0])
+                {
+                    _currentWayPoint = _wayPoints[1];
+                    _agent.SetDestination(_currentWayPoint.position);
+                }
+                else if (_wayPoints[2] != null)
+                {
+                    _currentWayPoint = _wayPoints[2];
+                    _agent.SetDestination(_currentWayPoint.position);
+                }
+
+            }
+        }
+    }
 
 }
