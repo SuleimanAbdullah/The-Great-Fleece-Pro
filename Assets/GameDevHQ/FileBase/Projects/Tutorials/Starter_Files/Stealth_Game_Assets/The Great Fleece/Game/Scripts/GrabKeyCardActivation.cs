74b61a5f2a81e7c9cddba84dc83221ef5b359e2e
SuleimanAbdullah <suleimanabdullahdev@gmail.com>
2022-10-06 14:51:40 +0300

    Fix camera Position ,Finishe sleeping guard Interaction

--- Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/Stealth_Game_Assets/The Great Fleece/Game/Scripts/GrabKeyCardActivation.cs
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/Stealth_Game_Assets/The Great Fleece/Game/Scripts/GrabKeyCardActivation.cs
@@ -6,12 +6,14 @@ public class GrabKeyCardActivation : MonoBehaviour
 {
     [SerializeField]
     private GameObject _sleepingGuardCutScene;
+    private bool _isCutSceneActivatedOnce;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Player")
+        if (other.tag == "Player" && _isCutSceneActivatedOnce == false)
         {
             _sleepingGuardCutScene.SetActive(true);
+            _isCutSceneActivatedOnce = true;
         }
     }
 }
