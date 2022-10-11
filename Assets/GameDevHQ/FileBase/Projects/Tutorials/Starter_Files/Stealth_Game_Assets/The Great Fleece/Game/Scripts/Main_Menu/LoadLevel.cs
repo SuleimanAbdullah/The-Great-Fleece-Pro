462ffe7ed4ef59978597022e055aceefd9042a19
SuleimanAbdullah <suleimanabdullahdev@gmail.com>
2022-10-11 12:22:42 +0300

    Implement Loading Bar in LoadingScene

---
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/Stealth_Game_Assets/The Great Fleece/Game/Scripts/Main_Menu/LoadLevel.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadLevel : MonoBehaviour
+{
+    // reference to progress bar
+
+    [SerializeField]
+    private Image _progressBar;
+
+    void Start()
+    {
+        //call coroutine to load Main Scene
+        StartCoroutine(LoadLevelAsync());
+    }
+
+    IEnumerator LoadLevelAsync()
+    {
+        //create asyncoperation = LoadLevelAsync("Main")
+        AsyncOperation operation = SceneManager.LoadSceneAsync("Main");
+        //while ooperation is not finished
+        while (operation.isDone == false)
+        {
+            _progressBar.fillAmount = operation.progress;
+            yield return null;
+        }
+          //progress bar fill amount = operaton progress
+          //wait end of the frame
+    }
+}
--- Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/Stealth_Game_Assets/The Great Fleece/Game/Scripts/Main_Menu/MainMenu.cs
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/Stealth_Game_Assets/The Great Fleece/Game/Scripts/Main_Menu/MainMenu.cs
@@ -8,7 +8,6 @@ public class MainMenu : MonoBehaviour
     public void StartGame()
     {
         SceneManager.LoadScene("Main");
-        Debug.Log("AM I Crazy");
     }
 
     public void QuitGame()
